9d5464a2a3bc073e187e2e913e445c44563aa2fd
cfiander <cfiander90@gmail.com>
2019-07-12 16:27:02 -0700

    api revert

--- src/Recipes/Recipes-Router.js
+++ src/Recipes/Recipes-Router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const RecipeService = require('./Recipes-Service')
 const recipeRouter = express.Router()
 const unirest = require ('unirest')
+const config = require('../config')
 
 const jsonBodyParser = express.json()
 
@@ -13,7 +14,7 @@ recipeRouter
    
     unirest.get(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=20&ranking=1&ignorePantry=false&ingredients=${ingredients}`)
     .header("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
-    .header("X-RapidAPI-Key", "db5a101104mshcf69cbd8b65f653p1ddc40jsn7f91d36c4804")
+    .header("X-RapidAPI-Key", `${config.API_TOKEN}`)
     .end(function (result) {
     res.status(200).send(result.body);
   })
@@ -25,7 +26,7 @@ recipeRouter
         const {recipeId} = req.body
         unirest.get(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${recipeId}/information`)
         .header("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
-        .header("X-RapidAPI-Key", "db5a101104mshcf69cbd8b65f653p1ddc40jsn7f91d36c4804")
+        .header("X-RapidAPI-Key", `${config.API_TOKEN}`)
         .end(function (result) {
             res.status(200).json(result.body.sourceUrl);
         });
--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
+  // EXTERNAL_API: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=20&ranking=1&ignorePantry=false&ingredients=`,
   PORT: process.env.PORT || 8000,
+  API_TOKEN: process.env.API_TOKEN || 'none',
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/blogful',
   JWT_SECRET: process.env.JWT_SECRET || 'my-new-secret',
