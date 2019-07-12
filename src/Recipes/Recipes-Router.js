c4b980253e940620c1eaed41a7be0cc719fd691d
cfiander <cfiander90@gmail.com>
2019-07-12 02:52:36 -0700

    deploy

--- src/Recipes/Recipes-Router.js
+++ src/Recipes/Recipes-Router.js
@@ -3,22 +3,33 @@ const RecipeService = require('./Recipes-Service')
 const recipeRouter = express.Router()
 const unirest = require ('unirest')
 
-
+const jsonBodyParser = express.json()
 
 recipeRouter
   .route('/')
-    .get((req, res, next) => {
+    .post(jsonBodyParser, (req, res, next) => {
     // const { number, ranking, ignorePantry = null, ingredients = [] } = req.query;
-    const {ingredients} = req.query
-    console.log(ingredients)
-    unirest.get(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=${ingredients}`)
+    const {ingredients} = req.body
+   
+    unirest.get(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=20&ranking=1&ignorePantry=false&ingredients=${ingredients}`)
     .header("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
     .header("X-RapidAPI-Key", "db5a101104mshcf69cbd8b65f653p1ddc40jsn7f91d36c4804")
     .end(function (result) {
-        console.log(result.body)
     res.status(200).send(result.body);
   })
   })
+
+recipeRouter
+  .route(`/url`)
+    .post(jsonBodyParser, (req, res, next) => {
+        const {recipeId} = req.body
+        unirest.get(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${recipeId}/information`)
+        .header("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
+        .header("X-RapidAPI-Key", "db5a101104mshcf69cbd8b65f653p1ddc40jsn7f91d36c4804")
+        .end(function (result) {
+            res.status(200).json(result.body.sourceUrl);
+        });
+    })
 
   module.exports = recipeRouter
 
---
+++ src/Recipes/Recipes-Service.js
@@ -0,0 +1,21 @@
+// const express = require('express')
+
+// const app = express()
+
+// const RecipeService = {
+//     getRecipeImage(result) {
+//         const imageArray = result.body.map(ingredient => {
+//             const innerArray = [];
+//             innerArrray.push(`https://spoonacular.com/recipeImages/`)
+//             innerArrray.push(ingredient.id)
+//             innerArrray.push(`636x393`)
+//             innerArray.push(ingredient.imageType)
+//             innerArray.push(ingredient.image)
+//             innerArray.join('')
+//             imageArray.push(innerArray)
+//         })
+//         return imageArray   
+// },
+// }
+
+// module.exports = RecipeService
