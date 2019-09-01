06acb640f5613642803a764dd7bda78558604cdb
cfiander <cfiander90@gmail.com>
2019-09-01 16:55:25 -0700

    fix

--- src/Recipes/Recipes-Router.js
+++ src/Recipes/Recipes-Router.js
@@ -11,7 +11,7 @@ recipeRouter
     // const { number, ranking, ignorePantry = null, ingredients = [] } = req.query;
     console.log(req.body)
     const {ingredients} = req.body
-    unirest.get(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=50&ranking=1s&ignorePantry=false&ingredients=${ingredients}`)
+    unirest.get(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=50&ranking=1&ignorePantry=false&ingredients=${ingredients}`)
     .header("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
     .header("X-RapidAPI-Key", `${config.API_TOKEN}`)
     .end(function (result) {
